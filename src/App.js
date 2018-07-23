50933666c6296570bd19284e1af4fd1a0e68bd08
Young Koh <digitalkoh@users.noreply.github.com>
2018-07-23 01:00:04 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'; 
 import Route from 'react-router-dom/Route';
 // import uuid from 'uuid';
-//import $ from 'jquery';
+// import $ from 'jquery';
 
 import Modal from 'react-modal';
 import data from './data/cardContent.json';
 
 import './stylesheets/App.css';
 import Cards from './Components/Card/Cards';
+import Related from './Components/Related/Related';
 import SlidePane from './Components/SlidePane';
 import HeaderBar from './Components/Header/HeaderBar';
+import Banner from './Components/Header/Banner';
+import Footer from './Components/Footer/Footer';
 
+let contentStyle = {
+  position: 'absolute',
+  margin: 'auto',
+  width: '100%',
+  transform: 'translate(0%, -6%)'
+}
 
 class App extends Component {
 
@@ -48,14 +57,26 @@ class App extends Component {
 
           {/* <Route path='/' component={Nav} /> */}
 
+          <Route path='/' component={HeaderBar} />
+
           <Route exact strict path='/dashboard' render={
             ()=> {
               return(
                 <div>
-                  <HeaderBar />
-
                   <main>
-                    <Cards cardsdata={this.state.cardsdata} />
+
+                    <Banner />
+
+                    <div className="content" style={contentStyle}>
+
+                      <Cards cardsdata={this.state.cardsdata} />
+
+                      <div className="Related-block"><Related /></div>
+                      
+                      <Footer />
+                      
+                    </div>
+
                   </main>
                 </div>
               )
